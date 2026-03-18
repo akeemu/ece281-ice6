a5bc97b13fbc2e56fe744a7f78cb3b9b84d38cc4
akeemu <c28isaac.kim@afacademy.af.edu>
2026-03-17 21:48:09 -0600

    Initial commit

--- src/TDM4.vhd
+++ src/TDM4.vhd
@@ -49,7 +49,7 @@
 --|    s_<signal name>          = state name
 --|
 --+----------------------------------------------------------------------------
-library IEEE;
+library IEEE; 
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
